0499630a2e2a48cdb9987611e0bf440294a9a772
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-04-23 16:39:23 +0200

    Inlined string.cpp

--- src/string.hpp
+++ src/string.hpp
@@ -3,6 +3,16 @@
 
 #include <stdint.h>
 
-extern "C" uint_fast32_t __attribute__ ((pure)) strlen(const char *str);
+extern "C" inline uint_fast32_t __attribute__ ((pure)) strlen(const char *str)
+{
+   uint_fast32_t res = 0;
+
+   while (*str) {
+      ++str;
+      ++res;
+   }
+
+   return res;
+}
 
 #endif
