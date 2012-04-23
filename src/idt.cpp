fa382db3bb9a459e32b754f51d18608ed05cfc7e
Leo Ekleog Gaspard <ekleog@gmail.com>
2012-04-23 16:33:15 +0200

    Made idt.cpp functions inline

--- src/idt.cpp
+++ src/idt.cpp
@@ -1,45 +1,4 @@
 #include "idt.hpp"
 
-#include <stdint.h>
-
-struct idt_elem
-{
-   uint16_t low_offset;
-   uint16_t selector;
-   uint16_t settings;
-   uint16_t high_offset;
-};
-
-struct idtr
-{
-   uint16_t limit;
-   idt_elem *base;
-} __attribute__ ((packed));
-
-static idtr IDTR;
-static idt_elem IDT[256];
-
-void init_idt()
-{
-   IDTR.limit  = 256 * sizeof(idt_elem);
-   IDTR.base   = IDT;
-   asm volatile ("lidt %0" : : "m"(IDTR));
-}
-
-void isr(uint8_t id, void (*isr) (), uint8_t dpl)
-{
-   uint16_t selector;
-   asm volatile ("movw %0, %%cs" : "=g"(selector));
-
-   uint32_t offset = reinterpret_cast<uint32_t>(isr);
-
-   IDT[id].low_offset   = offset & 0xFFFF;
-   IDT[id].selector     = selector;
-   IDT[id].settings     = 0x8E00 | static_cast<uint16_t>(dpl << 13);
-   IDT[id].high_offset  = static_cast<uint16_t>(offset >> 16);
-}
-
-void validate_idt()
-{
-   asm volatile ("sti");
-}
+idtr IDTR;
+idt_elem IDT[256];
--- src/idt.hpp
+++ src/idt.hpp
@@ -3,10 +3,46 @@
 
 #include <stdint.h>
 
-void init_idt();
+struct idt_elem
+{
+   uint16_t low_offset;
+   uint16_t selector;
+   uint16_t settings;
+   uint16_t high_offset;
+};
 
-void isr(uint8_t id, void (*isr) (), uint8_t dpl);
+struct idtr
+{
+   uint16_t limit;
+   idt_elem *base;
+} __attribute__ ((packed));
 
-void validate_idt();
+extern idtr IDTR;
+extern idt_elem IDT[256];
+
+inline void init_idt()
+{
+   IDTR.limit  = 256 * sizeof(idt_elem);
+   IDTR.base   = IDT;
+   asm volatile ("lidt %0" : : "m"(IDTR));
+}
+
+inline void isr(uint8_t id, void (*isr) (), uint8_t dpl)
+{
+   uint16_t selector;
+   asm volatile ("movw %0, %%cs" : "=g"(selector));
+
+   uint32_t offset = reinterpret_cast<uint32_t>(isr);
+
+   IDT[id].low_offset   = offset & 0xFFFF;
+   IDT[id].selector     = selector;
+   IDT[id].settings     = 0x8E00 | static_cast<uint16_t>(dpl << 13);
+   IDT[id].high_offset  = static_cast<uint16_t>(offset >> 16);
+}
+
+inline void validate_idt()
+{
+   asm volatile ("sti");
+}
 
 #endif
